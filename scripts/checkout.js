e843883f62a6d60a0b1af857808f78eacd64b9a0
Sawant-Saish <work.saishsawant@gmail.com>
2026-02-23 22:04:36 +0530

    Convert Products into classes

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,6 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js"; 
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
-import '../data/carts-class.js';
+//import '../data/carts-class.js';
 
 renderOrderSummary(); 
 renderPaymentSummary();
