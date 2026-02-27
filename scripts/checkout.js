48e133527cb294ec68c9c07d5c0625e6e4814c59
Sawant-Saish <work.saishsawant@gmail.com>
2026-02-27 17:57:05 +0530

    learn promises and convert code to use promises

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -4,7 +4,27 @@ import { loadproducts } from "../data/products.js";
 //import '../data/carts-class.js';
 //import '../data/backend-practice.js';
 
+new Promise((resolve)=>{
+    loadproducts(() => {
+        resolve();
+    });
+}).then(()=>{
+    renderOrderSummary(); 
+    renderPaymentSummary();     
+});
+
+// this promise does the same thing as the below callback function which is it waits for the loadproducts to load before moving to next steps i.e renderOrdersummary and renderPaymentSummary. 
+
+// promise and callback both makes the async flow to be in sync.
+
+//  Promises allows us avoid nesting and helpls us keep the code clean. 
+// it still has features like promise.all() which lets us run multiple promises at the same time. 
+
+
+
+/*
 loadproducts(()=>{
     renderOrderSummary(); 
     renderPaymentSummary();
 });
+*/
